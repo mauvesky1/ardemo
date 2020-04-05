4806a842df62120568468be949ec5f66257f099b
Norbs89 <n_huvos@live.co.uk>
2020-04-05 13:21:46 +0100

    saving the butterfly count into a refObj and displaying results on screen

--- ar.js
+++ ar.js
@@ -1,4 +1,29 @@
-var targetEl = document.querySelector("#butterfly");
+const targetEl = document.querySelector("#butterfly");
+const textEl = document.querySelector("#text");
+const refObj = {};
+
 targetEl.addEventListener("click", function() {
-  console.log("clicked");
+  addToRefObj(targetEl);
+  console.log(refObj);
+  textEl.setAttribute("text", "value", `Caught: ${refObj[targetEl.id]}`);
+  targetEl.components.animation.attrValue.enabled = true;
 });
+
+const addToRefObj = target => {
+  if (refObj[target.id]) {
+    refObj[target.id] = refObj[target.id] + 1 || 0;
+  } else {
+    refObj[target.id] = 1;
+  }
+  return refObj;
+};
+
+//how to create new element on page!
+
+// const newEntity = document.createElement("a-entity");
+// newEntity.setAttribute("geometry", {
+//   primitive: "box",
+//   height: 3,
+//   width: 1
+// });
+// targetEl.appendChild(newEntity);
