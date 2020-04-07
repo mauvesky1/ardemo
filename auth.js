e6cdb043342601691afc8002f8af9d512d3c3fd8
bob <rob.draper@live.com>
2020-04-07 17:03:41 +0100

    added add user

--- auth.js
+++ auth.js
@@ -3,4 +3,9 @@ const signupButton = document.querySelector("#sign");
 
 signupButton.addEventListener("click", (e) => {
   console.log("button clicked!");
+  auth
+    .createUserWithEmailAndPassword("rob.draper@live.com", "chnrchnr")
+    .then((cred) => {
+      console.log("user", cred);
+    });
 });
